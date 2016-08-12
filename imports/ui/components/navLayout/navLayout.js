5b3f835ceadd6a6a3f63ea322799dabac2baa4ec
jefferson <jeffersondesouzacordel@gmail.com>
2016-08-12 02:55:42 -0300

    NAV LATERAL, Botoes de acesso a paginas de crud escondidos. \n proximo passo, Negar acesso a essas páginas

--- imports/ui/components/navLayout/navLayout.js
+++ imports/ui/components/navLayout/navLayout.js
@@ -1,9 +1,35 @@
 import './navLayout.module.js';
 
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import {Meteor} from 'meteor/meteor';
+
+import {Usuarios} from '../../../api/usuarios/index.js';
+
 //Template
 import navLayout from './navLayout.html';
 
-class NavLayout{}
+
+class NavLayout{
+  constructor($stateParams, $scope, $reactive) {
+      'ngInject';
+
+      $reactive(this).attach($scope);
+    this.subscribe('usuarios',()=>{},{
+      onStart: function () {
+      }
+    });
+  }
+
+  isFuncionario(){
+    let username = Meteor.users.findOne(Meteor.userId()).username;
+
+    if(Usuarios.findOne({matricula:username}).categoriaUsuario === 'funcionario'){
+      return true;
+    }
+    return false;
+  }
+}
 
 const name = 'navLayout';
 
@@ -11,5 +37,3 @@ export default angular.module(name).component(name,{
         templateUrl:'imports/ui/components/navLayout/navLayout.html',
         controller: NavLayout
     });
-
-
